79d567b3a566b9a5b6eb8e8d6251452685b46ec9
yancarq <yancarlosrq@gmail.com>
2021-03-06 08:11:08 +0000

    Ejercicio de todo list

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+//Todo List
 export function Home() {
+	const [listTodo, setListTodo] = useState([]);
+
+	function EventEnter(evento) {
+		if (evento.key === "Enter" || evento.keyCode === 13) {
+			AddTodoList(evento);
+		}
+	}
+	function AddTodoList(evento) {
+		let valor = evento.target.value;
+		if (valor != "") {
+			setListTodo(listTodo => listTodo.concat(valor));
+			evento.target.value = "";
+		} else {
+			alert("Santo guacamole! Debe ingresar una tarea.!!!");
+		}
+	}
+
+	function DeleteTodo(posicion) {
+		let lista = listTodo;
+		console.log(lista);
+
+		lista.splice(posicion, 1);
+		console.log(lista);
+
+		setListTodo(listTodo => lista.concat());
+	}
+
+	function DrawTodoList() {
+		const li = listTodo.map((value, index) => {
+			return (
+				<li className="list-group-item" key={index}>
+					{value}
+					<button
+						type="button"
+						className="close text-danger"
+						aria-label="Close"
+						onClick={() => DeleteTodo(index)}>
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</li>
+			);
+		});
+
+		return (
+			<div>
+				<ul className="list-group">{li}</ul>
+			</div>
+		);
+	}
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container">
+				<div className="text-center mt-5">
+					<h1 className="display-2 alert alert-primary">To Dos</h1>
+				</div>
+				<div className="card">
+					<div className="card-body">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Agregar tareas por hacer!!!"
+							// onBlur={e => AgregarTodoLista(e)}
+							onKeyUp={e => EventEnter(e)}
+						/>
+						<DrawTodoList />
+						<p className="text-secondary mt-3">
+							{listTodo.length} Item left
+						</p>
+					</div>
+				</div>
+				<div className="panelFootFirst"></div>
+				<div className="panelFootSecond"></div>
+			</div>
+		</>
 	);
 }
