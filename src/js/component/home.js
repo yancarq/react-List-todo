c2ee8eea82e3fce650f5e99344953affbd0671cc
yancarq <yancarlosrq@gmail.com>
2021-03-11 06:19:36 +0000

    list fetch

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,39 +1,143 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //Todo List
 export function Home() {
+	const url = "https://assets.breatheco.de/apis/fake/todos/user/yancarq";
+
 	const [listTodo, setListTodo] = useState([]);
 
-	function EventEnter(evento) {
-		if (evento.key === "Enter" || evento.keyCode === 13) {
-			AddTodoList(evento);
+	useEffect(() => {
+		CreateUserApi();
+		GetTodoList();
+	}, []);
+
+	useEffect(() => {
+		CreateUserApi();
+		GetTodoList();
+
+		async function UpdateTodolist() {
+			console.log(listTodo);
+			console.log(listTodo.length + " -- ");
+			fetch(url, {
+				method: "PUT",
+				body: JSON.stringify(listTodo),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(resp => resp.json())
+				.then(data => {
+					GetTodoList();
+					console.log(data);
+				})
+				.catch(function(error) {
+					//manejo de errores
+					console.log("error", error.message);
+					console.log("Error");
+				});
 		}
-	}
+
+		UpdateTodolist();
+	});
+
 	function AddTodoList(evento) {
 		let valor = evento.target.value;
 		if (valor != "") {
-			setListTodo(listTodo => listTodo.concat(valor));
+			let task = {
+				label: valor,
+				done: false
+			};
+			setListTodo(listTodo => listTodo.concat(task));
 			evento.target.value = "";
+			console.log(listTodo.length);
 		} else {
 			alert("Santo guacamole! Debe ingresar una tarea.!!!");
 		}
 	}
 
+	function EventEnter(evento) {
+		if (evento.key === "Enter" || evento.keyCode === 13) {
+			AddTodoList(evento);
+			console.log(listTodo.length + " -- ");
+			//UpdateTodolist();
+		}
+	}
+
 	function DeleteTodo(posicion) {
 		let lista = listTodo;
-		console.log(lista);
-
-		lista.splice(posicion, 1);
-		console.log(lista);
-
+		//	lista.splice(posicion, 1);
+		lista[posicion].done = true;
 		setListTodo(listTodo => lista.concat());
+		console.log(lista);
+	}
+
+	//Conectar a la API Fetch
+	function CreateUserApi() {
+		fetch(url, {
+			method: "POST",
+			body: [],
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => resp.json())
+			.then(data => {
+				console.log(data);
+			})
+			.catch(function(error) {
+				//manejo de errores
+				console.log("error", error.message);
+				console.log("Error");
+			});
+	}
+
+	function UpdateTodolist() {
+		console.log(listTodo);
+
+		fetch(url, {
+			method: "PUT",
+			body: JSON.stringify(listTodo),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => resp.json())
+			.then(data => {
+				GetTodoList();
+				console.log(data);
+			})
+			.catch(function(error) {
+				//manejo de errores
+				console.log("error", error.message);
+				console.log("Error");
+			});
+	}
+
+	function GetTodoList() {
+		fetch(url, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => resp.json())
+			.then(data => {
+				let listaResultado = data.filter(elem => elem.done == false);
+				console.log(listaResultado);
+				setListTodo(listTodo => listaResultado);
+			})
+			.catch(function(error) {
+				//manejo de errores
+				console.log("error", error.message);
+				console.log("Error");
+			});
 	}
 
 	function DrawTodoList() {
 		const li = listTodo.map((value, index) => {
 			return (
 				<li className="list-group-item" key={index}>
-					{value}
+					{value.label}
 					<button
 						type="button"
 						className="close text-danger"
@@ -56,7 +160,7 @@ export function Home() {
 		<>
 			<div className="container">
 				<div className="text-center mt-5">
-					<h1 className="display-2 alert alert-primary">To Do's</h1>
+					<h1 className="display-2 alert alert-primary">To Dos</h1>
 				</div>
 				<div className="card">
 					<div className="card-body">
@@ -79,3 +183,51 @@ export function Home() {
 		</>
 	);
 }
+
+// function CreateTodoList() {
+// 	fetch(url, {
+// 		method: "POST",
+// 		body: [],
+// 		headers: {
+// 			"Content-Type": "application/json"
+// 		}
+// 	})
+// 		.then(resp => {
+// 			console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+// 			console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+// 			console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+// 			return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+// 		})
+// 		.then(data => {
+// 			//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+// 			console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+// 		})
+// 		.catch(error => {
+// 			//manejo de errores
+// 			console.log(error);
+// 		});
+// }
+
+function DeleteTodoList() {}
+
+// fetch(url, {
+// 	method: "POST",
+// 	body: JSON.stringify(todos),
+// 	headers: {
+// 		"Content-Type": "application/json"
+// 	}
+// })
+// 	.then(resp => {
+// 		console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+// 		console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+// 		console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+// 		return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+// 	})
+// 	.then(data => {
+// 		//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+// 		console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+// 	})
+// 	.catch(error => {
+// 		//manejo de errores
+// 		console.log(error);
+// 	});
